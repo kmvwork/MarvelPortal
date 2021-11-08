4c686653763d00cfe0537fe5e9547ecb59211291
Maksim <maksimus-94@tut.by>
2021-11-08 22:35:50 +0300

    Add Random Char timer

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -19,6 +19,15 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService()
 
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 10000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
     onCharLoaded = (char) => {
         this.setState({char, loading: false})
     }
@@ -65,9 +74,14 @@ const View = ({char}) => {
 
     const {name, description, thumbnail, homepage, wiki} = char
 
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
